234c697b11c866d3496d5421e87eb1ed666093ad
ArshakVardanov <103132731+ArshakVardanov@users.noreply.github.com>
2022-05-10 21:39:32 +0400

    Creared Welcome Page

---
+++ Welcome.swift
@@ -0,0 +1,11 @@
+//
+//  Welcome.swift
+//  Zoo
+//
+//  Created by Arshak Vardanov on 10.05.22.
+//
+
+import UIKit
+
+class WelcomPage: UIView {
+    
---
+++ Zoo/main.swift
@@ -0,0 +1,7 @@
+//
+//  main.swift
+//  Zoo
+//
+//  Created by Arshak Vardanov on 10.05.22.
+//
+
